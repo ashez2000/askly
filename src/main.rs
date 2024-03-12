432ff053a29350003ef08562769b92f100a6d9a2
Aswin Gangadharan <aswin.gangadharan.2020@gmail.com>
2024-03-13 01:37:22 +0530

    Setup warp server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
-fn main() {
-    println!("Hello, world!");
+use warp::Filter;
+
+#[tokio::main]
+async fn main() {
+    let hello = warp::get().map(|| format!("Hello, world!"));
+
+    warp::serve(hello).run(([127, 0, 0, 1], 3000)).await;
 }
