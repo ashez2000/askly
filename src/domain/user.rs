4956ed5fccf4c4d8207be6c0c31b5650af1d4c19
Aswin Gangadharan <aswin.gangadharan.2020@gmail.com>
2024-03-16 20:37:17 +0530

    Add user model

---
+++ src/domain/user.rs
@@ -0,0 +1,17 @@
+use serde::{Deserialize, Serialize};
+use uuid::Uuid;
+
+#[derive(Debug, Clone, Serialize)]
+pub struct User {
+    pub id: Uuid,
+    pub name: String,
+    pub email: String,
+    pub password: String,
+}
+
+#[derive(Debug, Clone, Deserialize)]
+pub struct NewUser {
+    pub name: String,
+    pub email: String,
+    pub password: String,
+}
